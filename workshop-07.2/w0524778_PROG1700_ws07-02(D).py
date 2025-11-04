ad03b777fdac0ec8b98488e6a444d8610f912d65
tobin <tobin.chisholm@gmail.com>
2025-11-04 15:07:19 -0400

    still coding D

--- workshop-07.2/w0524778_PROG1700_ws07-02(D).py
+++ workshop-07.2/w0524778_PROG1700_ws07-02(D).py
@@ -1,7 +1,7 @@
 songs = ["Song A", "Song B", "Song C"]
 plays = [5, 10, 7]
 least_played_song = ""
-least_played_play = 0
+least_played = 100
 most_played_song = ""
 most_played = 0
 #loop to print all songs with play counts
@@ -9,5 +9,10 @@ for i in range(len(songs)):
     print(f"{songs[i]}: {plays[i]}")
     if plays[i] > most_played:
         most_played = plays[i]
-        most_played_song = plays[i]
+        most_played_song = songs[i]
+    if plays[i] > least_played:
+        least_played = plays[i]
+        least_played_song = songs[i]
+print(f"The most played song is {most_played_song} with {most_played} plays")
+print(f"the least played song is {least_played_song} wiht {least_played} plays")
     
