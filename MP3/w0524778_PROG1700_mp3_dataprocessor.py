76a2a5f6ef60bd5db905121f92152a7180ab8d63
tobin <tobin.chisholm@gmail.com>
2025-12-04 13:33:16 -0400

    yes

--- MP3/w0524778_PROG1700_mp3_dataprocessor.py
+++ MP3/w0524778_PROG1700_mp3_dataprocessor.py
@@ -9,8 +9,9 @@ def read_grades(file_path):
             grades[name] = int(grade)
     return grades
 
-def write_grades_to_txt_and_maths(grades, output):
-
+def write_grades_to_txt_and_maths_and_pass_or_fail(grades, output):
+    passing=[]
+    failing=[]
     total = 0
     highest = None
     lowest = None
@@ -23,17 +24,24 @@ def write_grades_to_txt_and_maths(grades, output):
                 highest = grade
             if lowest is None or grade < lowest:
                 lowest = grade
+            if grade < 50:
+                failing.append(name)
+            else:
+                passing.append(name)
 
         avg = total / len(grades)
         f.write(f"Average: {avg:.2f}\n")
         f.write(f"Highest grade: {highest}\n")
         f.write(f"Lowest grade: {lowest}\n")
+        f.write(f"Passing grade: {passing}\n")
+        f.write(f"Failing grade: {failing}\n")
+        
 
 
 
 def main():
     grades_dict = read_grades("grades.csv")
-    write_grades_to_txt_and_maths(grades_dict, "report.txt")
+    write_grades_to_txt_and_maths_and_pass_or_fail(grades_dict, "report.txt")
     print("The report can be found in report.txt")
 
 if __name__ == "__main__":
